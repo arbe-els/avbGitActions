2c330d186dc4c9b0791c67a71fe73e7454f306ba
Artur B <a.berehovyi@elsevier.com>
2025-04-28 16:41:19 +0200

    Update first_run.py

--- first_run.py
+++ first_run.py
@@ -1,8 +1,11 @@
+# this is only for PyLint green
 def add_operation(a, b):
+    # this is only for PyLint green
     return a + b
 
 
 def subtract_operation(a, b):
+    # this is only for PyLint green
     return a - b
 
 
