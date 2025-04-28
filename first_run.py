e72833176e6464a4e1306f1c4cb9a49ec4a8eb8f
Artur B <a.berehovyi@elsevier.com>
2025-04-28 17:22:18 +0200

    Update first_run.py

--- first_run.py
+++ first_run.py
@@ -1,9 +1,13 @@
 """ this is only for PyLint green light """
-def add_operation(a, b): """ this is only for PyLint green light """
+
+
+def add_operation(a, b):
+    """this is only for PyLint green light"""
     return a + b
 
 
-def subtract_operation(a, b): """ this is only for PyLint green light """
+def subtract_operation(a, b):
+    """this is only for PyLint green light"""
     return a - b
 
 
