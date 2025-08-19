2facea5da20fd392ebb262375b7b368272bf588b
artbereh <a.berehovyi@elsevier.com>
2025-08-19 17:31:38 +0200

    commit from local 3f

--- first_run.py
+++ first_run.py
@@ -10,6 +10,10 @@ def subtract_operation(a, b):
     """this is only for PyLint green check"""
     return a - b
 
+def make_an_erroras(do):
+    """dog pet animal catpillar"""
+    return do-23
+
 
 if __name__ == '__main__':
     print("Pepsi Company")
